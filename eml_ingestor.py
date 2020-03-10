0ff8663f3d208b863708855af16e3780d1f177d4
ryry311 <rgpowell@calpoly.edu>
2020-03-09 18:11:16 -0700

    Random tweaks to data ingestor.

--- eml_ingestor.py
+++ eml_ingestor.py
@@ -5,7 +5,7 @@ from email import parser
 
 def main():
    msgs = get_msgs_as_strings()
-   #clean_msgs(msgs)
+   clean_msgs(msgs)
 
    # print example email body
    random.shuffle(msgs)
@@ -13,12 +13,12 @@ def main():
 
 def clean_msgs(msgs):
    # there's probably a better way to remove these, but I haven't found it
-   bad_line_prefixes = ['Received:', 'Return-Path:', 'Message-ID:', 'Content-Transfer-Encoding:', 'X-IronPort-Anti-Spam-Filtered:', 'X-IronPort-Anti-Spam-Result:', 'X-IronPort-AV:', 'X-Originating-IP-Address:', 'To:', 'Subject:', 'From:', 'Date:', 'Content-Type:', 'MIME-Version:']
+   bad_line_prefixes = ['Received:', 'Return-Path:', 'Message-ID:', 'Content-Transfer-Encoding:', 'X-IronPort-Anti-Spam-Filtered:', 'X-IronPort-Anti-Spam-Result:', 'X-IronPort-AV:', 'X-Originating-IP-Address:', 'To:', 'Subject:', 'From:', 'Date:', 'Content-Type:', 'MIME-Version:', 'Content-Transfer-Encoding:']
 
    for i in range(len(msgs)):
       new_msg = msgs[i]
-      #while ('<' in new_msg and '>' in new_msg):
-      #   new_msg = re.sub(r'\<.*\>', '', new_msg)
+      while ('<' in new_msg and '>' in new_msg):
+         new_msg = re.sub(r'\<[^>]*\>', '', new_msg)
       new_msg = new_msg.split('\n')
       for line in new_msg:
          for pref in bad_line_prefixes:
@@ -42,6 +42,7 @@ def get_msgs_as_strings():
                   num_parts += 1
             if num_parts > 0:
                msgs.append(as_string)
+            f.close()
          except UnicodeDecodeError:
             # for some reason "conf_emails/[GAMESNETWORK] CFP + 3rd keynote_ DiGRA 2020 - Frans Mäyrä (TAU) <frans.mayra@TUNI.FI> - 2019-10-31 0239.eml" is unreadable
             pass
