576cb97f78cd91eff2f422329e330da0840df384
ryry311 <rgpowell@calpoly.edu>
2020-03-16 13:35:00 -0700

    Some more data ingestion tweaks.

--- eml_ingestor.py
+++ eml_ingestor.py
@@ -1,31 +1,38 @@
 import os
 import random
 import re
+from nltk.tag import StanfordNERTagger
 from email import parser
 
 def main():
    msgs = get_msgs_as_strings()
    clean_msgs(msgs)
 
+   st = StanfordNERTagger('english.muc.7class.distsim.crf.ser.gz', '/Users/RyanPowell/Downloads/stanford-ner-2018-10-16/stanford-ner.jar')
+   print(st.tag(msgs[0].split()))
+
    # print example email body
-   random.shuffle(msgs)
-   print(msgs[0])
+   #random.shuffle(msgs)
+   #print(find_ners(msgs[0]))
 
 def clean_msgs(msgs):
-   # there's probably a better way to remove these, but I haven't found it
-   bad_line_prefixes = ['Received:', 'Return-Path:', 'Message-ID:', 'Content-Transfer-Encoding:', 'X-IronPort-Anti-Spam-Filtered:', 'X-IronPort-Anti-Spam-Result:', 'X-IronPort-AV:', 'X-Originating-IP-Address:', 'To:', 'Subject:', 'From:', 'Date:', 'Content-Type:', 'MIME-Version:', 'Content-Transfer-Encoding:']
-
    for i in range(len(msgs)):
       new_msg = msgs[i]
       while ('<' in new_msg and '>' in new_msg):
          new_msg = re.sub(r'\<[^>]*\>', '', new_msg)
       new_msg = new_msg.split('\n')
-      for line in new_msg:
-         for pref in bad_line_prefixes:
-            if pref in line:
-               if line in new_msg:
-                  new_msg.remove(line)
+      for j in range(len(new_msg)):
+         if has_bad_prefix(new_msg[j]):
+            new_msg[j] = ''
       msgs[i] = '\n'.join(new_msg)
+
+def has_bad_prefix(line):
+   # there's probably a better way to remove these, but I haven't found it
+   bad_line_prefixes = ['Received:', 'Return-Path:', 'Message-ID:', 'Content-Transfer-Encoding:', 'X-IronPort-Anti-Spam-Filtered:', 'X-IronPort-Anti-Spam-Result:', 'X-IronPort-AV:', 'X-Originating-IP-Address:', 'To:', 'Subject:', 'From:', 'Date:', 'Content-Type:', 'MIME-Version:', 'Content-Transfer-Encoding:', 'X-IPAS-Result:']
+   for pref in bad_line_prefixes:
+      if pref in line:
+         return True
+   return False
 
 def get_msgs_as_strings():
    msgs = []
