62cf6849560acfb2df87dd691cf75f721a4f3340
Austin Silveria <austinsilveria@gmail.com>
2020-03-16 14:13:34 -0700

    Add full UI (#2)

--- web/ui/src/App.tsx
+++ web/ui/src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Layout, Menu, Breadcrumb, Button } from "antd";
 import './App.css';
 import 'antd/dist/antd.css';
 
 import Event from "./components/Event";
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Event></Event>
-      </header>
-    </div>
+      <Layout className="layout">
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <div className="logo">Event Extractor</div>
+        </Header>
+    <Content style={{ padding: '0 50px', marginTop: 114 }}>
+      <div className="site-layout-content">
+        <Event/>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>CSC 482 Speech and Language Processing</Footer>
+  </Layout>
   );
 }
 
